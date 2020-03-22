2407c080f1734a029f878b8b6472cbf7235d27fe
nvhoor <nvhoor@gmail.com>
2020-03-23 00:06:50 +0700

    [up] View Model and Repository v2

--- src/AspNetCoreSpa.Core/Entities/Province.cs
+++ src/AspNetCoreSpa.Core/Entities/Province.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AspNetCoreSpa.Core.Entities
 {
-    public class Provinces : IEntityBase
+    public class Province : IEntityBase
     {
         [Key]
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         [Column(TypeName = "NVARCHAR(100)")]
         public string Name { get; set; }
-        public decimal Latitude { get; set; }
         public decimal Longitude { get; set; }
+        public string Latitude { get; set; }
+        public ICollection<Tour> Tours { get; set; }
+        public ICollection<TourProgram> TourPrograms { get; set; }
     }
 }
--- src/AspNetCoreSpa.Infrastructure/SeedData.cs
+++ src/AspNetCoreSpa.Infrastructure/SeedData.cs
@@ -36,6 +36,7 @@ namespace AspNetCoreSpa.Infrastructure
             _context.Database.Migrate();
 
             AddLocalisedData();
+            AddTourData();
            // AddShopData();
         }
         private void AddLocalisedData()
@@ -77,6 +78,198 @@ namespace AspNetCoreSpa.Infrastructure
             }
         }
 
+        private void AddTourData()
+        {
+            
+            if (!_context.PostCategories.Any())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _context.PostCategories.Add(new PostCategory
+                    {
+                        Name = "Category " + i,
+                    });
+                }
+
+                _context.SaveChanges();
+            }
+
+            if (!_context.Posts.Any())
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    _context.Posts.Add(new Post
+                    {
+                        Name = "Post " + i,
+                        PostContent = "Post content " + i,
+                        Description = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+                            Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet",
+                        MetaDescription = "Post Meta description " + i,
+                        MetaKeyWord = "Post Meta description " + i,
+                        Alias = "Post description " + i,
+                        Status = true,
+                        Censorship = true,
+                        Deleted = false,
+                        PostCategoryId =  new Random().Next(1, 11),
+                        CreatedDate = DateTime.UtcNow,
+                        UpdatedDate = DateTime.UtcNow
+                    });
+                }
+                _context.SaveChanges();
+            }
+            if (!_context.Contacts.Any())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _context.Contacts.Add(new Contact
+                    {
+                        FullName = "Contact " + i,
+                        Email = $"{i}test@test.com",
+                        Address = "Address " + i,
+                        Phone = "037489202"+i,
+                        Title = "Content Tile Contact "+i
+                    });
+                }
+                _context.SaveChanges();
+            }
+            if (!_context.Prices.Any())
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    _context.Prices.Add(new Price
+                    {
+                        Name = "Post " + i,
+                        PromotionPrice = new Random().Next(100000,200000),
+                        OriginalPrice = new Random().Next(1000000,5000000),
+                        StartDatePro = DateTime.UtcNow,
+                        TouristTypeId = new Random().Next(1,4)
+                    });
+                }
+                _context.SaveChanges();
+            }
+            // if (!_context.Provincess.Any())
+            // {
+            //     for (int i = 0; i < 20; i++)
+            //     {
+            //         _context.Prices.Add(new Price
+            //         {
+            //             Name = "Post " + i,
+            //             PromotionPrice = new Random().Next(100000,200000),
+            //             OriginalPrice = new Random().Next(1000000,5000000),
+            //             StartDatePro = DateTime.UtcNow,
+            //             TouristTypeId = new Random().Next(1,4)
+            //         });
+            //     }
+            //     _context.SaveChanges();
+            // }
+            if (!_context.Tours.Any())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _context.Tours.Add(new Tour
+                    {
+                        Name = "Post " + i,
+                        Description = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+                            Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet",
+                        DepartureDate = DateTime.UtcNow,
+                        Status = true,
+                        Censorship = true,
+                        Deleted = false,
+                        Slot = new Random().Next(1,20),
+                        DepartureId = new Random().Next(1,4),
+                        CategoryId = new Random().Next(1,4)
+                    });
+                }
+                _context.SaveChanges();
+            }
+            if (!_context.TourBookings.Any())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _context.TourBookings.Add(entity: new TourBooking
+                    {
+                        FullName = "Tour Booking " + i,
+                        Email = $"{i}test@test.com",
+                        Address = "Address " + i,
+                        Mobile = "037489202"+i,
+                        Note = "Note " + i,
+                        Status = true,
+                        Deleted = false,
+                        UserId = new Random().Next(1,10),
+                    });
+                }
+                _context.SaveChanges();
+            }
+            if (!_context.TourBookingDetails.Any())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _context.TourBookingDetails.Add(new TourBookingDetail
+                    {
+                        TourId = new Random().Next(1,4),
+                        TourBookingId = new Random().Next(1,4)
+                    });
+                }
+                _context.SaveChanges();
+            }
+            if (!_context.TourCategories.Any())
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    _context.TourCategories.Add(new TourCategory()
+                    {
+                        Name = "Post " + i,
+                        Description = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+                            Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet",
+                    });
+                }
+                _context.SaveChanges();
+            }
+            if (!_context.TourCustomers.Any())
+            {
+                             for (int i = 0; i < 10; i++)
+                             {
+                                 _context.TourCustomers.Add(new TourCustomer
+                                 {
+                                     FullName = "Tony " +i,
+                                     BirthDay = DateTime.Now.AddDays(i),
+                                     Gender = i % 2 == 0 ? Gender.Male : Gender.Female,
+                                     TourBookingId = new Random().Next(1,5),
+                                     TouristTypeId = new Random().Next(1,4)
+                                 });
+                             }
+                             _context.SaveChanges();
+            }
+            if (!_context.TouristTypes.Any())
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    _context.TouristTypes.Add(new TouristType
+                    {
+                        Name = "Touristype " +i,
+                    });
+                }
+                _context.SaveChanges();
+            }
+            if (!_context.TourPrograms.Any())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _context.TourPrograms.Add(new TourProgram
+                    {
+                        Date = DateTime.Now.AddDays(i),
+                        OrderNumber = 1 + i,
+                        Title = @"Lorem ipsum dolor sit amet Nulla quis sem at nibh elemen",
+                        Description = @"Lorem ipsum dolor sit amet Nulla quis sem at nibh elemen",
+                        DestinationId = new Random().Next(1,4),
+                        TourId = new Random().Next(1,10)
+                        
+                    });
+                }
+                _context.SaveChanges();
+            }
+            
+        }
         private void AddShopData()
         {
 //            if (!_context.Customers.Any())
